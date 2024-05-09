2273925899eebd7705af210e05059c92de70a930
LukaDokovic <dokovic68@gmail.com>
2024-05-09 11:56:29 +0200

    09.05.2024

---
+++ model.py
@@ -0,0 +1,11 @@
+import torch
+print(torch.__version__)
+
+
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
+
+# Use the model
+model.train(data="config.yaml", epochs=1)  # train the model
